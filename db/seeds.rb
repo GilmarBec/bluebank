8d4074a9235038ab88c600a3015809ba89cf4f2a
GilmarBec <trabalhos4.5.1.4@gmail.com>
2019-03-08 00:30:23 -0300

    Update to production configs
    Heroku requirements.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ employees = [
 	{
 		name: "Administrador",
 		email: "trabalhos4.5.1.4@gmail.com",
+		agency: "104",
+		office: "Desenvolvedor",
 		login: "Admin",
 		password: "asd123",
 		password_confirmation: "asd123"
@@ -22,22 +24,28 @@ employees.each do |employee|
 	Employee.create employee
 end
 
-## Addresses
-addresses = [
+accounts = [
 	{
-		cep: "88106785",
-		state: "SC",
-		city: "São José",
-		district: "Forquilhinhas",
-		street: "Alguma",
-		number: 649
+		name: "Gilmar",
+		cpf: "00000000000",
+		agency: "104",
+		number: 000000,
+		password: "Admin123",
+		current_amount: 30000,
+		credit: 2000,
+		token: "",
+	},
+	{
+		name: "Pedro",
+		cpf: "00000000001",
+		agency: "105",
+		number: 000001,
+		password: "Pedro123",
+		current_amount: 80000,
+		credit: 10,
+		token: "",
 	}
 ]
-addresses.each do |address|
-  Address.create address
+accounts.each do |account|
+  Account.create account
 end
-
-## Agencies
-agencies = [
-	number: 101
-]
